1babc591852d2f30ea2b60e4c4ff30b101476721
Eduardo Santos <eduardosantosmcontato@gmail.com>
2026-03-07 18:57:11 -0300

    first commit

---
+++ src/main/java/br/com/duduzyn/java10x_course/Java10xCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.duduzyn.java10x_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java10xCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java10xCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/duduzyn/java10x_course/Java10xCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duduzyn.java10x_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java10xCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
