6cd629c6abca0a203ff90c773912af7140691951
Eduardo Santos <eduardosantosmcontato@gmail.com>
2026-03-07 19:13:18 -0300

    Adiciona Controller.java

---
+++ src/main/java/br/com/duduzyn/java10x_course/Controller.java
@@ -0,0 +1,18 @@
+package br.com.duduzyn.java10x_course;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Primeira mensagem dessa rota";
+    }
+
+
+
+}
