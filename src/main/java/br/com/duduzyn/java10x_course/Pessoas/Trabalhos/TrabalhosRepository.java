d31b8a9fc9813f7749f8d825bb0494d996463788
Eduardo Santos <eduardosantosmcontato@gmail.com>
2026-03-10 15:28:30 -0300

    feat: Comecando a trabalhar a interface Repository de trabalho

---
+++ src/main/java/br/com/duduzyn/java10x_course/Pessoas/Trabalhos/TrabalhosRepository.java
@@ -0,0 +1,5 @@
+package br.com.duduzyn.java10x_course.Pessoas.Trabalhos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrabalhosRepository extends JpaRepository<TrabalhosModel, Long> {
+}
